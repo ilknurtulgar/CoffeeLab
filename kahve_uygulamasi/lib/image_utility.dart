b3a4b509c6144744659daefb113b90ff4021c653
GizemGoksu <gizemgks_282@hotmail.com>
2022-11-02 20:26:39 +0300

    fotoğrafların yüklenmesi ve imageUtility hazırlanması

---
+++ kahve_uygulamasi/lib/image_utility.dart
@@ -0,0 +1,13 @@
+class ImageUtility {
+  String caramelMacchiatoAsset = "assets/images/cMacchiato.jpeg";
+  String whiteChocolateMochaAsset = "assets/images/wcMocha.jpeg";
+  String mochaAsset = "assets/images/mocha.jpeg";
+  String conPannaAsset = "assets/images/conPanna.jpeg";
+  String frappe = "assets/images/frappe.jpeg";
+  String iceAmericanoAsset = "assets/images/iceAmericano.jpeg";
+  String latteAsset = "assets/images/latte.jpeg";
+  String cappucinoAsset = "assets/images/cappucino.jpeg";
+  String macchiatoAsset = "assets/images/macchiato.jpeg";
+  String flatWhiteAsset = "assets/images/flatWhite.jpeg";
+  String espressoAsset = "assets/images/espresso.jpeg";
+}
