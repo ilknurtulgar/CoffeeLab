aabc568efe672e6d6c593fc9abfc8b323b1449cc
jjasminn <jasminn247@outlook.com>
2022-11-03 16:18:30 +0300

    Utilitys_color

---
+++ kahve_uygulamasi/lib/core/Utilitys/base_utility.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color Koromiko = Color(0xFFFFB067);
+  static Color White = Color(0xFFFFFFFF);
+  static Color Black = Color(0xFF000000);
+  static Color WildSand = Color(0xFFF5F5F5);
+  static Color ApricotPeach = Color(0xFFFBDABC);
+  static Color Tacao = Color(0xFFEDBD90);
+  static Color MineShaft = Color(0xFF333333);
+  static Color Merino = Color(0xFFF4ECE4);
+}
